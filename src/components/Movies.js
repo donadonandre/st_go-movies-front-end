678bdb599be4cf25e492fcfec71990e7b8550309
André Donadon <andreltdonadon@gmail.com>
2023-12-30 17:06:26 -0300

    finalizing fake movies

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,9 +1,51 @@
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+
 const Movies = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        let moviesList = [
+            {
+                id: 1,
+                title: "Highlander",
+                release_date: "1986-03-07",
+                runtime: 116,
+                mpaa_rating: "R",
+                description: "Some description here"
+            }
+        ]
+
+        setMovies(moviesList)
+    }, []);
+
     return (
         <>
             <div className="text-center">
                 <h2>Movies</h2>
                 <hr/>
+                <table className="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Movie</th>
+                            <th>Release Date</th>
+                            <th>Rating</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {movies.map((m) => (
+                        <tr key={m.id}>
+                            <td>
+                                <Link to={`/movies/${m.id}`}>
+                                    {m.title}
+                                </Link>
+                            </td>
+                            <td>{m.release_date}</td>
+                            <td>{m.mpaa_rating}</td>
+                        </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </>
     )

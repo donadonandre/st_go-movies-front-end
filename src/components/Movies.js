30171f2d9a04a2f937c02b97424c6fdbda0045ab
André Donadon <andreltdonadon@gmail.com>
2023-12-30 23:09:35 -0300

    (FIX) Movies.js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -24,34 +24,34 @@ const Movies = () => {
     }, []);
 
     return (
-        <>
-            <div className="text-center">
-                <h2>Movies</h2>
-                <hr/>
-                <table className="table table-striped table-hover">
-                    <thead>
-                        <tr>
-                            <th>Movie</th>
-                            <th>Release Date</th>
-                            <th>Rating</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {movies.map((m) => (
-                        <tr key={m.id}>
-                            <td>
-                                <Link to={`/movies/${m.id}`}>
-                                    {m.title}
-                                </Link>
-                            </td>
-                            <td>{m.release_date}</td>
-                            <td>{m.mpaa_rating}</td>
-                        </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </>
+
+        <div>
+            <h2>Movies</h2>
+            <hr/>
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Movie</th>
+                        <th>Release Date</th>
+                        <th>Rating</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {movies.map((m) => (
+                    <tr key={m.id}>
+                        <td>
+                            <Link to={`/movies/${m.id}`}>
+                                {m.title}
+                            </Link>
+                        </td>
+                        <td>{m.release_date}</td>
+                        <td>{m.mpaa_rating}</td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+
     )
 }
 

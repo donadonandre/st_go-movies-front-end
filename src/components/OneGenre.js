d6d78e9b63b7577769f02251b9b865b0852e902f
André Donadon <andreltdonadon@gmail.com>
2024-01-07 17:31:17 -0300

    Creating OneGenre method

---
+++ src/components/OneGenre.js
@@ -0,0 +1,66 @@
+import {Link, useLocation, useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+
+const OneGenre = () => {
+    const location = useLocation();
+    const {genreName} = location.state;
+
+    const [movies, setMovies] = useState([]);
+
+    let {id} = useParams();
+
+    useEffect(() => {
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json");
+
+        const requestOptions = {
+            method: "GET",
+            headers: headers,
+        }
+
+        fetch(`/movies/genres/${id}`, requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.error) {
+                    console.log(data.error);
+                } else {
+                  setMovies(data);
+                }
+            }).catch((err) => console.log(err))
+    }, [id]);
+
+    return (
+        <>
+            <h2>Genre: {genreName}</h2>
+            <hr/>
+            {movies ? (
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Movie</th>
+                        <th>Release Date</th>
+                        <th>Rating</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {movies.map((m) => (
+                    <tr key={m.id}>
+                        <td>
+                            <Link to={`/movies/${m.id}`}>
+                                {m.title}
+                            </Link>
+                        </td>
+                        <td>{m.release_date}</td>
+                        <td>{m.mpaa_rating}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            ) : (
+                <p>No movies in this genre (yet)!</p>
+            )}
+        </>
+    )
+}
+
+export default OneGenre;
